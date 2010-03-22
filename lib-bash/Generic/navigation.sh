78dc8b26547bdc7243ea3542f2785f3b499115d7
Katrina Payne <fullmetalharlot@nimhlabs.com>
2010-03-22 01:09:49 -0600

    simple command that can be called in .bashrc, rather than pasted into it

---
+++ lib-bash/Generic/navigation.sh
@@ -0,0 +1,19 @@
+
+# meh, this appears in enough bash setups, could do its own file.
+
+# Let us add colours
+alias ls="ls --color=auto"
+
+# Let us add the symbols on thme
+alias ls="ls -F"
+
+# more verbose listing
+alias ll="ls -l"
+
+# show hidden
+alias la="ls -A"
+
+# hidden _and_ verbose
+alias lal="ls -lA"
+
+
